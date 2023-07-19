fc4b87c01cb9f1cf23ddeb14f6dbd543a7d0e794
yashr <rshailu.yash@gmail.com>
2023-07-19 14:15:39 -0400

    add homepage

---
+++ src/app/main/home.js
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div className="w-full h-screen text-center">
+        <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center item-center rounded-3xl bg-lightgrey">
+            <p className="uppercase text-sm tracking-widest text-darkergrey">Welcome to The Banan Project</p>
+        </div>    
+    </div>
+  )
+}
