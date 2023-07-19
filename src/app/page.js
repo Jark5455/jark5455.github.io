2a63dd103b6fea67c8782fb124b45a957d1df3d9
yashr <rshailu.yash@gmail.com>
2023-07-19 15:19:13 -0400

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import HomePage from './main/home';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between no-scrollbar">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <NavBar />
       <Background />
 
