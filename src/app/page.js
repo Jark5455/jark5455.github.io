0f262752f7174bed7e0e34d57388b752e64b512b
yashr <rshailu.yash@gmail.com>
2023-07-19 14:26:02 -0400

    absolute positioning

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import Background from './particles-bg';
 import NavBar from './navbar/navbar';
 import HomePage from './main/home';
@@ -6,9 +5,6 @@ import HomePage from './main/home';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between no-scrollbar">
-        <Head>
-        </Head>
-
         <NavBar />
         <Background>
           <HomePage />
--- src/app/particles-bg.js
+++ src/app/particles-bg.js
@@ -4,7 +4,7 @@ export default function Background() {
     return (
         <div>
             <Script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js" />
-            <div id="particles-bg" className="w-screen h-screen bg-yellow bg-cover bg-center bg-no-repeat -z-10"/>
+            <div id="particles-bg" className="absolute top-0 left-0 w-screen h-screen bg-yellow bg-cover bg-center bg-no-repeat -z-10"/>
             <Script src="/js/bg.js" />
         </div>
     )
