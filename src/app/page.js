5bf43afdc7a5582fc8f7cb0e6aa0521ed4c883cb
yashr <rshailu.yash@gmail.com>
2023-07-19 14:26:36 -0400

    page out of child

--- src/app/page.js
+++ src/app/page.js
@@ -7,8 +7,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between no-scrollbar">
         <NavBar />
         <Background>
-          <HomePage />
         </Background>
+        <HomePage />
     </main>
   )
 }
