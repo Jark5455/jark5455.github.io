b77a844620c09b8544b601bd541b7ba73918168d
yashr <rshailu.yash@gmail.com>
2023-07-19 22:01:21 -0400

    use client

--- src/app/main/home.js
+++ src/app/main/home.js
@@ -1,3 +1,5 @@
+"use client"
+
 import { useState } from "react";
 
 export default function HomePage() {
@@ -13,7 +15,7 @@ export default function HomePage() {
 
 
   return (
-    <div className={`relative w-full h-fit text-center mt-[calc(${navBarSize}px+24px)]`}>
+    <div className={`relative w-full h-fit text-center top-[calc(${navBarSize}px+24px)]`}>
         <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center item-center rounded-3xl bg-lightgrey">
             <p className="uppercase text-sm tracking-widest text-darkergrey">Welcome to The Banan Project</p>
         </div>    
