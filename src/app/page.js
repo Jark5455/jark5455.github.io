4add9e698d5503baca421235d04cc0e75db5dd2e
Jark5455 <rshailu.yash@gmail.com>
2023-07-23 00:19:33 -0400

    revert

--- src/app/page.js
+++ src/app/page.js
@@ -4,15 +4,13 @@ import HomePage from './main/home';
 
 export default function Home() {
   return (
-    <div className="w-full h-full">
+    <main className="flex min-h-screen h-fit flex-col items-center justify-between">
       <Background />
-      <main className="flex min-h-screen flex-col items-center justify-between">
-        <NavBar />
+      <NavBar />
 
-        <div className="absolute flex top-[125px] w-fit h-fit" >
-          <HomePage />
-        </div>
-      </main>
-    </div>
+      <div className="absolute flex top-[125px] w-fit h-fit" >
+        <HomePage />
+      </div>
+    </main>
   )
 }
