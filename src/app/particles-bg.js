d8862dc830b31a4f9f09328ebbf78fed7240f557
Yash Rachamalla <39651378+Jark5455@users.noreply.github.com>
2025-03-22 19:39:24 -0400

    Update particles-bg.js

--- src/app/particles-bg.js
+++ src/app/particles-bg.js
@@ -3,8 +3,7 @@ import Script from 'next/script';
 export default function Background() {
   return (
     <div id="particles-bg" className="absolute flex top-0 bottom-0 right-0 left-0 bg-yellow bg-cover bg-center bg-no-repeat -z-10">
-      <Script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js" />
-      <Script src="/js/bg.js" />
+      <Script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js" onload="particlesJS.load('particles-js', 'json/particles.json');" />
     </div>
   )
 }
