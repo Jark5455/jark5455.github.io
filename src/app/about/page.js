91f50d956f414c22646ffcd9c8c869b90df17edc
rshailu.yash@gmail.com <rshailu.yash@gmail.com>
2025-05-12 14:32:25 -0400

    fixed about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -4,16 +4,21 @@ import React from "react";
 
 import Content from "./about.mdx";
 
+import Background from "../particles-bg";
+
 export default function About() {
   return (
-    <main className="relative grow items-center justify-between">
-      <div className="m-auto p-[25px]">
-        <div className="w-full h-full">
-          <div className="max-w-[1240px] mx-auto min-h-[80vh] p-20 items-center rounded-3xl bg-darkergrey">
-            <Content />
+    <>
+      <Background />
+      <main className="relative grow items-center justify-between">
+        <div className="m-auto p-[25px]">
+          <div className="w-full h-full">
+            <div className="max-w-[1240px] mx-auto min-h-[80vh] p-20 items-center rounded-3xl bg-darkergrey">
+              <Content />
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
