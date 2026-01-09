0a4cd4201ed93d4e2dde9c9e9c8ffced317aac95
Yash Rachamalla <rshailu.yash@gmail.com>
2026-01-09 18:27:56 -0500

    replaced particles.js with tsparticles

--- src/app/particles-bg.js
+++ src/app/particles-bg.js
@@ -1,35 +1,34 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
-import Script from "next/script";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
 
 export default function Background() {
-  const pathname = usePathname();
-  const [isScriptReady, setIsScriptReady] = useState(false);
+  const [init, setInit] = useState(false);
 
   useEffect(() => {
-    if (isScriptReady && window.particlesJS) {
-      const container = document.getElementById("particles-bg");
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
 
-      if (container) {
-        container.innerHTML = "";
-      }
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
 
-      window.particlesJS.load("particles-bg", "/json/particles.json");
-    }
-  }, [pathname, isScriptReady]);
-
-  return (
-    <div
-      id="particles-bg"
-      className="absolute flex top-0 bottom-0 right-0 left-0 bg-yellow bg-cover bg-center bg-no-repeat -z-10"
-    >
-      <Script
-        defer
-        src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js"
-        onReady={() => setIsScriptReady(true)}
+  if (init) {
+    return (
+      <Particles
+        id="tsparticles"
+        className="absolute flex top-0 bottom-0 right-0 left-0 bg-yellow bg-cover bg-center bg-no-repeat -z-10"
+        particlesLoaded={particlesLoaded}
+        url="/json/particles.json"
       />
-    </div>
-  );
+    );
+  }
+
+  return <></>;
 }
